cee8cdd20abbd7dfe4ec3de7c8538d95536e29f6
Mihail Lisin <mslfoxgit@gmail.com>
2024-12-31 10:15:38 +0600

    для алексей

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,7 @@ void setup()
   uint8_t macAddress[6];                      // Буфер для хранения MAC-адреса (6 байт)
   esp_read_mac(macAddress, ESP_MAC_WIFI_STA); // Чтение MAC-адреса Wi-Fi станции (ESP32)
 
-  // 0 MY)
-  uint8_t targetMacAddress[] = {0xEC,0x64,0xC9,0x5E,0x65,0xFC}; 
-
-  // Вывод полученного MAC-адреса в монитор последовательного порта
-  // Serial.print("\nMAC Address: ");
-  // for (int i = 0; i < 6; i++)
-  // {
-  //   Serial.printf("%02X", macAddress[i]); // Вывод каждого байта в шестнадцатеричном виде
-  //   if (i < 5)
-  //   {
-  //     Serial.print(":"); // Добавление двоеточия между байтами (кроме последнего)
-  //   }
-  // }
-  // Serial.println(); // Переход на новую строку
+  uint8_t targetMacAddress[] = {0xEC, 0x64, 0xC9, 0x5E, 0x65, 0xFC};
 
   delay(500);
   setup_EEPROM();
@@ -35,7 +22,7 @@ void setup()
   if (memcmp(macAddress, targetMacAddress, 6) == 0)
   {
 #endif
-runServer();
+    runServer();
     Serial.println("Setup Done");
 #ifdef MAC_CHECK
   }
